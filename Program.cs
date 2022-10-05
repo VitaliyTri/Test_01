417fa2559c316c4c7d0ab47b2fedeccfb307d829
Vitaliy <V.tal@mail.ru>
2022-10-05 16:39:30 +0700

    program written

--- Program.cs
+++ Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿string[] array = { "df", "2377", "384", "4500", "FSB", "GB" };
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        count++;
+    }
+}
+string[] newArray = new string[count];
+int j = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3 && j < count)
+    {
+        newArray[j] = array[i];
+        Console.Write(newArray[j] + "\t");
+        j++;
+    }
+}
